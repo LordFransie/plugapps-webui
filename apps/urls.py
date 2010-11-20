8f269053341f10ee6ac2d78d38e53c22d4c84602
mrsteveman1 <mrsteveman1@gmail.com>
2010-11-20 14:46:53 -0600

    fix up some of the app page stuff, change it to be static since there are only going to be a few apps for now

--- apps/urls.py
+++ apps/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+	(r'^samba', 'apps.views.samba'),
+	(r'^minidlna', 'apps.views.minidlna'),
 	(r'^$', 'apps.views.index'),
+
 )
--- apps/views.py
+++ apps/views.py
@@ -3,18 +3,16 @@ from django.http import HttpRequest
 import os
 import privateapi.core
 
-def render(filename,**variables):
-    context = globals()
-    context.update(variables)
-    from gluon.template import render
-    return render(filename=os.path.join(request.folder,'views',filename),
-                  path=os.path.join(request.folder,'views'),
-                  context=context,delimiters=('{%','%}'))
-
 def index(request): 
     installed_apps = os.listdir("/etc/installed_apps/")
     return render_to_response('apps/index.html', { "installed_apps": installed_apps })
 
-def package(request):
-    package_name = request.GET['name']
-    return HttpRequest('apps/' + package_name + '.html')
+def minidlna(request): 
+    installed_apps = os.listdir("/etc/installed_apps/")
+    return render_to_response('apps/minidlna.html', { "installed_apps": installed_apps })
+
+def samba(request): 
+    installed_apps = os.listdir("/etc/installed_apps/")
+    return render_to_response('apps/samba.html', { "installed_apps": installed_apps })
+
+
