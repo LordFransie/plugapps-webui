899247b5e64fea15007a61664fc349af343dbac6
mrsteveman1 <mrsteveman1@gmail.com>
2010-11-21 12:30:01 -0600

    quick port of the pacman module to support apt

---
+++ privateapi/apt.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python 
+# coding: utf8
+
+import shlex, subprocess, re, socket
+
+def check():
+    base_aptgetupdateupgrade_command_raw = "sudo apt-get update && sudo apt-get upgrade"
+    args = shlex.split(base_pacmansyup_command_raw)
+    process = subprocess.Popen(args,stdout=subprocess.PIPE,universal_newlines=True)
+    output_list = []
+    for line in process.stdout.readlines():
+        newoutput = line.rstrip('\n')
+        if not "0 upgraded, 0 newly installed, 0 to remove and 0 not upgraded." in newoutput:
+            continue
+        elif "0 upgraded, 0 newly installed, 0 to remove and 0 not upgraded." in newoutput:
+            return False
+    return True
+           
+def list_upgrades():
+    base_aptgetupgrade_command_raw = "sudo apt-get upgrade"
+    args = shlex.split(base_aptgetupgrade_command_raw)
+    process = subprocess.Popen(args,stdout=subprocess.PIPE,universal_newlines=True)
+    output_list = []
+    for line in process.stdout.readlines():
+        newoutput = line.rstrip('\n')
+        output_list.append(newoutput)
+    return output_list
+
+def doupdateos():
+       base_upgrade_command_raw = "sudo apt-get -y upgrade"
+       args = shlex.split(base_upgrade_command_raw)
+       process = subprocess.Popen(args,stdout=subprocess.PIPE,universal_newlines=True)
+       output_list = []
+       for line in process.stdout.readlines():
+              newoutput = line.rstrip('\n')
+              output_list.append(newoutput)
+              output_list.append('<br/>')
+       return output_list
