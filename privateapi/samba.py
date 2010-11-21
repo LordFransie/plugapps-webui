c7da66462ccad0573ae5bc850d85ebdbdd884544
mrsteveman1 <mrsteveman1@gmail.com>
2010-11-21 12:36:29 -0600

    fix up samba controls so they work

--- privateapi/samba.py
+++ privateapi/samba.py
@@ -1,7 +1,42 @@
 #!/usr/bin/env python 
 # coding: utf8
 
-import shlex, subprocess
+import shlex, subprocess, re, socket, os
 
-def get_config():
-    pass
+def is_installed():
+    fpath = "/usr/sbin/smbd"
+    return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+           
+def is_running():
+    fpath = "/var/run/samba/smbd.pid"
+    return os.path.isfile(fpath)
+
+def start():
+    if is_installed():
+       base_start_command_raw = "sudo /etc/rc.d/samba start"
+       args = shlex.split(base_start_command_raw)
+       process = subprocess.Popen(args,stdout=subprocess.PIPE,universal_newlines=True)
+       for line in process.stdout.readlines():
+            newoutput = line.rstrip('\n')
+            if not ":: Starting Samba Server" in newoutput:
+                continue
+            elif ":: Starting Samba Server" in newoutput:
+                return True
+       return False
+    else:
+        return False
+
+def stop():
+    if is_installed():
+       base_stop_command_raw = "sudo /etc/rc.d/samba stop"
+       args = shlex.split(base_stop_command_raw)
+       process = subprocess.Popen(args,stdout=subprocess.PIPE,universal_newlines=True)
+       for line in process.stdout.readlines():
+            newoutput = line.rstrip('\n')
+            if not ":: Stopping Samba Server" in newoutput:
+                continue
+            elif ":: Stopping Samba Server" in newoutput:
+                return True
+       return False
+    else:
+        return False
