368dc793995f84d4b9925edbfa54ecd423939274
mrsteveman1 <mrsteveman1@gmail.com>
2010-11-25 21:53:51 -0600

    implement minidlna config parser and display config in browser, remove is_installed checks because if youre seeing the page for an app, its already installed

--- apps/views.py
+++ apps/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-
+from django import forms
 import os
 import privateapi.core
+import privateapi.minidlna
+import privateapi.samba
 
-from django.contrib.auth.decorators import login_required
+class MinidlnaForm(forms.Form):
+	strict_dlna = forms.CharField()
+	enable_tivo = forms.CharField()
+	#album_art_names = forms.CharField(max_length=300)
+	media_dir = forms.CharField()
+	serial = forms.CharField()
+	port = forms.CharField()
+
+
+
+
 
 @login_required
 def index(request): 
@@ -13,8 +26,13 @@ def index(request):
 
 @login_required
 def minidlna(request): 
-    installed_apps = os.listdir("/etc/installed_apps/")
-    return render_to_response('apps/minidlna.html', { "installed_apps": installed_apps },context_instance=RequestContext(request))
+	if request.method == 'POST':
+		pass
+	else:
+		installed_apps = os.listdir("/etc/installed_apps/")
+		config_dict = privateapi.minidlna.get_config()
+		form = MinidlnaForm(initial=config_dict)
+		return render_to_response('apps/minidlna.html', { "installed_apps": installed_apps, "form": form },context_instance=RequestContext(request))
 
 @login_required
 def samba(request): 
--- privateapi/minidlna.py
+++ privateapi/minidlna.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python 
 # coding: utf8
 
-import shlex, subprocess, re, socket, os
+import shlex, subprocess, re, socket, os, fileinput
 
 def is_installed():
     fpath = "/usr/sbin/minidlna"
@@ -40,3 +40,23 @@ def stop():
        return False
     else:
         return False
+	
+def get_config(file="/etc/minidlna.conf", delim='='):
+	d = {}
+
+	if file is None:
+		return -1
+
+	for line in fileinput.input(file):
+		if not line.strip(): # skip empty or space padded lines
+			continue
+		if re.compile('^#').search(line) is not None: # skip commented lines
+			continue
+		else: # pick up key and value pairs
+			kvp = line.strip().split(delim)
+			if kvp[1].strip().split('#') is not None:
+				d[kvp[0].strip()] = kvp[1].split('#')[0].strip()
+			else:
+				d[kvp[0].strip()] = kvp[1].strip()
+	return d
+		
